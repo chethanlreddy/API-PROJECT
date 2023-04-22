40e30bc32dba2a89b9186c34f67bdc2f1ef22a4c
chethanlreddy <chethanlreddy@outlook.com>
2023-04-22 21:08:52 +0530

    #

--- app/routers/super_user_operations.py
+++ app/routers/super_user_operations.py
@@ -6,11 +6,9 @@ from .. import schemas
 
 from .. import models, database, schemas, utils, oauth2
 router = APIRouter(
-    prefix='/fileinser',
+    prefix='/super_user',
     tags=['upload file']
 )
-@router.post('/',status_code=status.HTTP_201_CREATED)
-def insert_image(file_in : UploadFile = File(...)):
-    with open(f'{file_in.filename}', 'wb') as buffer:
-        shutil.copyfileobj(file_in.file,buffer)
-    return {'message' : file_in.filename}
+@router.post('/')
+def super_user_operations():
+    pass
